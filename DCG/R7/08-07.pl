32c0ab44b45c20d60adce24d59a1d5fe03b996a4
Mauricio-Arce <arce162011@gmail.com>
2015-05-01 13:18:27 -0600

    Regla arreglada

--- DCG/R7/08-07.pl
+++ DCG/R7/08-07.pl
@@ -1,2 +1,2 @@
-oracion --> verbo(Gv, Mv, Tv), pronombre(Gp, Mp, Tp), adjetivo(Gad, Mad, Tad), sustantivo(Gs, Ms, Ts), {N is Mv + Mp + Mp Mad + Ms},{N >= 5},{9 >= N },
-  {Gv = Gp}, {Gp = Gad}, {Gad = Gs}, {Tv = Tp}, {Tp = Tad}, {Tad = Ts}.
+oracion --> verbo(Mv, Tv), pronombre(Gp, Mp, Tp), adjetivo(Gad, Mad, Tad), sustantivo(Gs, Ms, Ts), {N is Mv + Mp + Mp Mad + Ms},{N >= 5},{9 >= N },
+ {Gp = Gad}, {Gad = Gs}, {Tv = Tp}, {Tp = Tad}, {Tad = Ts}.
