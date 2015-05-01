011964e6644ecbced95c810d138d9dc8b53c7e57
Mauricio-Arce <arce162011@gmail.com>
2015-05-01 13:14:13 -0600

    Regla arreglada

--- DCG/R5/06-03.pl
+++ DCG/R5/06-03.pl
@@ -1,1 +1,1 @@
-oracion --> pronombre(Gp, Mp, Tp), verbo(Mv, Tv), sustantivo(Gs, Ms, Ts), {N is Mp + Mv + Ms}, {N >= 3},{7 >= N }, {Gp = Gv}, {Gv = Gs}, {Tp = Tv}, {Tv = Ts}.
+oracion --> pronombre(Gp, Mp, Tp), verbo(Mv, Tv), sustantivo(Gs, Ms, Ts), {N is Mp + Mv + Ms}, {N >= 3},{7 >= N }, {Gp = Gs}, {Tp = Tv}, {Tv = Ts}.
