8cd9310396772a6487b889ddef9273b031f4dc61
Mauricio Arce <arce162011@gmail.com>
2015-05-01 15:07:57 -0600

    Reglas de 5 y 7 en un solo archivo

---
+++ DCG/Reglas5.pl
@@ -0,0 +1,9 @@
+oracion --> articulo(Ga, Ma, Ta), sustantivo(Gs, Ms, Ts), verbo(Mv,Tv), {Ga = Gs}, {N is Ma + Ms + Mv}, {N >= 3},{7 >= N }, {Ta = Ts}, {Ts = Tv}.
+oracion --> sustantivo(Gs, Ms, Ts), sustantivo(Gsu, Msu, Tsu), {N is Ms + Msu}, {N >= 3},{7 >= N }, {Gs = Gsu}, {Ts = Tsu}.
+oracion --> articulo(Ga, Ma, Ta), sustantivo(Gs, Ms, Ts), {N is Ma + Ms}, {N >= 3},{7 >= N }, {Ga = Gs}, {Ta = Ts}.
+oracion --> pronombre(Gp, Mp, Tp), verbo(Mv, Tv), {N is Mp + Mv}, {N >= 3},{7 >= N }, {Gp = Gv}, {Tp = Tv}.
+oracion --> sustantivo(Gs, Ms, Ts), articulo(Ga, Ma, Ta), adjetivo(Gad, Mad, Tad), {N is Ms + Ma + Mad}, {N >= 3},{7 >= N}, {Gs = Ga}, {Ga = Gad}, {Ts = Ta}, {Ta = Tad}.
+oracion --> pronombre(Gp, Mp, Tp), verbo(Mv, Tv), sustantivo(Gs, Ms, Ts), {N is Mp + Mv + Ms}, {N >= 3},{7 >= N }, {Gp = Gv}, {Gv = Gs}, {Tp = Tv}, {Tv = Ts}.
+oracion --> pronombre(Gp, Mp, Tp), articulo(Ga, Ma, Ta), sustantivo(Gs, Ms, Ts), {N is Mp + Ma + Ms}, {N >= 3},{7 >= N }, {Gp = Ga}, {Ga = Gs}, {Tp = Ta}, {Ta = Ts}.
+oracion --> verbo(Mv, Tv), articulo(Ga, Ma, Ta), sustantivo(Gs, Ms, Ts), {N is Mv + Ma + Ms}, {N >= 3},{7 >= N }, {Ga = Gs}, {Tv = Ta}, {Ta = Ts}.
+oracion --> verbo(Mv, Tv), sustantivo(Gs, Ms, Ts), {N is Mv + Ms}, {N >= 3},{7 >= N }, {Tv = Ts}.
