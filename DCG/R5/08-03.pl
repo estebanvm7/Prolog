bdb742b527c9ab94b1020a0dd728333e67731c9a
Mauricio-Arce <arce162011@gmail.com>
2015-05-01 12:31:53 -0600

    Regla arreglada

--- DCG/R5/08-03.pl
+++ DCG/R5/08-03.pl
@@ -1,1 +1,1 @@
-oracion --> verbo(Gv, Mv, Tv), articulo(Ga, Ma, Ta), sustantivo(Gs, Ms, Ts), {N is Mv + Ma + Ms}, {N >= 3},{7 >= N }, {Gv = Ga = Gs}, {Tv = Ta = Ts}.
+oracion --> verbo(Gv, Mv, Tv), articulo(Ga, Ma, Ta), sustantivo(Gs, Ms, Ts), {N is Mv + Ma + Ms}, {N >= 3},{7 >= N }, {Gv = Ga}, {Ga = Gs}, {Tv = Ta}, {Ta = Ts}.
