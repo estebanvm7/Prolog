8b60f9152e3e3b0e216529a174e4358e5c767d7c
Mauricio-Arce <arce162011@gmail.com>
2015-05-01 12:33:37 -0600

    Regla arreglada

--- DCG/R7/01-07.pl
+++ DCG/R7/01-07.pl
@@ -1,2 +1,2 @@
 oracion --> articulo(Ga, Ma, Ta), sustantivo(Gs, Ms, Ts), verbo(Mv, Tv), adjetivo(Gad, Mad, Tad), {N is Ma + Ms + Mv + Mad},
-	 {N >= 5},{9 >= N }, {Ga = Gs = Gad}, {Ta, Ts, Tv, Tad}.
+	 {N >= 5},{9 >= N }, {Ga = Gs}, {Gs = Gad}, {Ta = Ts}, {Ts = Tv}, {Tv = Tad}.
