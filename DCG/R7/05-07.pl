a318e1ac6d12a7874fc25ea79b28bdb949449a8e
Mauricio-Arce <arce162011@gmail.com>
2015-05-01 12:37:08 -0600

    Regla arreglada

--- DCG/R7/05-07.pl
+++ DCG/R7/05-07.pl
@@ -1,2 +1,3 @@
-oracion --> articulo(Gar, Mar, Tar), pronombre(Gp, Mp, Tp), sustantivo(Gs, Ms, Ts), articulo(Ga, Ma, Ta), {N is Mar + Mp + Ms + Ma},{N >= 5},{9 >= N }, {Gar = Gp = Gs = Ga}, {Tar = Tp =Ts = Ta}.
+oracion --> articulo(Gar, Mar, Tar), pronombre(Gp, Mp, Tp), sustantivo(Gs, Ms, Ts), articulo(Ga, Ma, Ta), {N is Mar + Mp + Ms + Ma},{N >= 5},{9 >= N }, 
+  {Gar = Gp}, {Gp = Gs}, {Gs = Ga}, {Tar = Tp}, {Tp = Ts}, {Ts = Ta}.
 
