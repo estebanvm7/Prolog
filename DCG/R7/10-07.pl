0b57ef673ef8880dd5110acfc26bcebd230f6c5c
Mauricio-Arce <arce162011@gmail.com>
2015-05-01 12:42:32 -0600

    Regla arreglada

--- DCG/R7/10-07.pl
+++ DCG/R7/10-07.pl
@@ -1,1 +1,2 @@
-oracion --> verbo(Gv, Mv, Tv), sustantivo(Gs, Ms, Ts), adjetivo(Gad, Mad, Tad), sustantivo(Gsu, Msu, Tsu), {N is Mv + Ms + Mad + Msu},{N >= 5},{9 >= N }, {Gv = Gs = Gad = Gsu}, {Tv = Ts = Tad = Tsu}.
+oracion --> verbo(Gv, Mv, Tv), sustantivo(Gs, Ms, Ts), adjetivo(Gad, Mad, Tad), sustantivo(Gsu, Msu, Tsu), {N is Mv + Ms + Mad + Msu},{N >= 5},{9 >= N },
+{Gv = Gs}, {Gs = Gad}, {Gad = Gsu}, {Tv = Ts}, {Ts = Tad}, {Tad = Tsu}.
