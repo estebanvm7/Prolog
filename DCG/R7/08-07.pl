a96ae7ff16022a1fa8390c212469931a7dee3575
Mauricio-Arce <arce162011@gmail.com>
2015-05-01 12:40:37 -0600

    Update 08-07.pl

--- DCG/R7/08-07.pl
+++ DCG/R7/08-07.pl
@@ -1,1 +1,2 @@
-oracion --> verbo(Gv, Mv, Tv), pronombre(Gp, Mp, Tp), adjetivo(Gad, Mad, Tad), sustantivo(Gs, Ms, Ts), {N is Mv + Mp + Mp Mad + Ms},{N >= 5},{9 >= N }, {Gv = Gp = Gad = Gs}, {Tv = Tp = Tad = Ts}.
+oracion --> verbo(Gv, Mv, Tv), pronombre(Gp, Mp, Tp), adjetivo(Gad, Mad, Tad), sustantivo(Gs, Ms, Ts), {N is Mv + Mp + Mp Mad + Ms},{N >= 5},{9 >= N },
+  {Gv = Gp}, {Gp = Gad}, {Gad = Gs}, {Tv = Tp}, {Tp = Tad}, {Tad = Ts}.
