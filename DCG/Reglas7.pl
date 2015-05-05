a0c0cad875293c8b05d8578587cd762350c7d536
Mauricio-Arce <arce162011@gmail.com>
2015-05-05 08:14:58 -0600

    Se alinio una regla(se quito un ENTER innecesario)

--- DCG/Reglas7.pl
+++ DCG/Reglas7.pl
@@ -3,8 +3,7 @@ oracion --> verbo(Mv, Tv), adjetivo(Gad, Mad, Tad), articulo(Ga, Ma, Ta), sustan
 oracion --> verbo(Mv, Tv), articulo(Ga, Ma, Ta), adjetivo(Gad, Mad, Tad), {N is Mv + Ma + Mad}, {N >= 5},{9 >= N }, {Ga= Gad}, {Tv = Ta}, {Ta = Tad}.
 oracion --> verbo(Mv, Tv), articulo(Ga, Ma, Ta), sustantivo(Gs, Ms, Ts), {N is Mv + Ma + Ms}, {N >= 5},{9 >= N }, {Ga = Gs}, {Tv = Ta}, {Ta = Ts}.
 oracion --> articulo(Gar, Mar, Tar), pronombre(Gp, Mp, Tp), sustantivo(Gs, Ms, Ts), articulo(Ga, Ma, Ta), {N is Mar + Mp + Ms + Ma},{N >= 5},{9 >= N }, {Gar = Gp}, {Gp = Gs}, {Gs = Ga}, {Tar = Tp}, {Tp = Ts}, {Ts = Ta}.
-oracion --> verbo(Mv, Tv), articulo(Ga, Ma, Ta), sustantivo(Gs, Ms, Ts), {N is Mv + Ma + Ms},{N >= 5},{9 >= N }, 
-{Ga = Gs}, {Tv = Ta}, {Ta = Ts}.
+oracion --> verbo(Mv, Tv), articulo(Ga, Ma, Ta), sustantivo(Gs, Ms, Ts), {N is Mv + Ma + Ms},{N >= 5},{9 >= N },{Ga = Gs}, {Tv = Ta}, {Ta = Ts}.
 oracion --> articulo(Ga, Ma, Ta), sustantivo(Gs, Ms, Ts), adjetivo(Gad, Mad, Tad), sustantivo(Gsu, Msu, Tsu), {N is Ma + Ms + Mad + Msu},{N >= 5},{9 >= N }, {Ga = Gs}, {Gs = Gad}, {Gad = Gsu}, {Ta = Ts}, {Ts = Tad},{Tad = Tsu}.
 oracion --> verbo(Mv, Tv), pronombre(Gp, Mp, Tp), adjetivo(Gad, Mad, Tad), sustantivo(Gs, Ms, Ts), {N is Mv + Mp + Mad + Ms},{N >= 5},{9 >= N }, {Gp = Gad}, {Gad = Gs}, {Tv = Tp}, {Tp = Tad}, {Tad = Ts}.
 oracion --> pronombre(Gp, Mp, Tp), verbo(Mv, Tv), sustantivo(Gs, Ms, Ts), {N is Mp + Mv + Ms}, {N >= 5},{9 >= N }, {Gp = Gs}, {Tp = Tv}, {Tv = Ts}.
